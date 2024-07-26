4e1c7998b6eabff1dac7017ce6603895f8c1f467
hamzaalighalib <97229996+hamzaalighalib@users.noreply.github.com>
2024-07-26 11:09:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const path = require("path");
 const axios = require("axios");
 const cheerio = require("cheerio");
-
+ 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const data = require("./hamza.json");
