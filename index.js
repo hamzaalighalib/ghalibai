6293394f776ac581b40fab8f8c350d4578e51b62
hamzaalighalib <97229996+hamzaalighalib@users.noreply.github.com>
2024-07-29 11:34:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const axios = require("axios");
 const data = require("./hamza.json");
 const OpenAI = require("openai");
 require("dotenv").config();
-
+ 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
